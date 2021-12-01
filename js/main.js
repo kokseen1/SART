99b2088e3393a65fad4a9ae3c85935adb4cdabc6
kose <53458630+kokseen1@users.noreply.github.com>
2021-12-01 21:48:27 +0800

    update: code refactoring, fixed seasonal/faves bugs

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
 const DOMAIN = "https://api.jikan.moe/v3/season";
 const WATCH_DOMAIN = "https://gogoanime.wiki//search.html?keyword=";
+const seasons = ["winter", "spring", "summer", "fall"];
+const getSeason = d => Math.floor((d.getMonth() / 12 * 4)) % 4;
 const now = new Date();
 
-let faves = [];
-var remain_faves_toggle;
-var seconds_arr = [];
-var functions_arr = [];
+let curr_year = now.getFullYear();
+let curr_season = getSeason(now);
+let get_season_text = () => seasons.at(curr_season % 4);
+let wrapper = document.getElementById("wrapper");
 
-var seasons = ['winter', 'spring', 'summer', 'fall'];
-var faves_toggle = false;
-var last_mode = null;
-var curr_year = now.getFullYear();
-const getSeason = d => Math.floor((d.getMonth() / 12 * 4)) % 4
-var curr_season = getSeason(now);
+let faves = [];
+let seconds_arr = [];
+let functions_arr = [];
+let faves_toggle = false;
+
+
+if ($.cookie("faves_toggle") == null) {
+    $.cookie("faves_toggle", "false", { expires: 9999 });
+} else {
+    faves_toggle = ($.cookie("faves_toggle") === "true");
+}
+
+if ($.cookie("faves") == null) {
+    $.cookie("faves", JSON.stringify(faves), { expires: 9999 });
+} else {
+    faves = JSON.parse($.cookie("faves"));
+}
+
 
 Date.prototype.addDays = function (days) {
     var date = new Date(this.valueOf());
@@ -20,6 +34,41 @@ Date.prototype.addDays = function (days) {
     return date;
 }
 
+
+let get_listings = () => {
+    let listings = document.querySelectorAll("[data-countdown]");
+    let listingsArray = Array.from(listings);
+    return listingsArray;
+};
+
+let soonest_sort = (a, b) => {
+    return (a.dataset.countdown - b.dataset.countdown);
+};
+
+let pop_sort = (a, b) => {
+    return (b.dataset.popularity - a.dataset.popularity);
+};
+
+let recent_sort = (a, b) => {
+    if (a.dataset.countdown > 604800) {
+        return 1;
+    } else if (b.dataset.countdown > 604800) {
+        return -1;
+    }
+    return Math.abs(604800 - a.dataset.countdown) - Math.abs(604800 - b.dataset.countdown);
+};
+
+let score_sort = (a, b) => {
+    if (a.dataset.score == "N/A") {
+        return 1;
+    } else if (b.dataset.score == "N/A") {
+        return -1;
+    } else {
+        return b.dataset.score - a.dataset.score;
+    }
+};
+
+let last_mode = soonest_sort;
 
 function retrieve_season(year, season) {
     let season_url = `${DOMAIN}/${year}/${season}`;
@@ -52,70 +101,33 @@ function retrieve_season(year, season) {
     return compiled_series_arr;
 };
 
-let get_listings = () => {
-    let listings = document.querySelectorAll("[data-countdown]");
-    let listingsArray = Array.from(listings);
-    return listingsArray;
-};
-let soonest_sort = (a, b) => {
-    return (a.dataset.countdown - b.dataset.countdown)
-};
-let pop_sort = (a, b) => {
-    return (b.dataset.popularity - a.dataset.popularity)
-};
-let recent_sort = (a, b) => {
-    if (a.dataset.countdown > 604800) {
-        return 1;
-    } else if (b.dataset.countdown > 604800) {
-        return -1;
-    }
-    return Math.abs(604800 - a.dataset.countdown) - Math.abs(604800 - b.dataset.countdown);
-};
-let score_sort = (a, b) => {
-    if (a.dataset.score == "N/A") {
-        return 1;
-    } else if (b.dataset.score == "N/A") {
-        return -1;
-    } else {
-        return b.dataset.score - a.dataset.score;
-    }
-};
 
-let sort_func = algo => {
+function sort_func(sort_mode = last_mode) {
     let listingsArray = get_listings();
     if (listingsArray.length) {
-        if (!algo) algo = score_sort;
-        let sortedListings = listingsArray.sort(algo);
+        let sortedListings = listingsArray.sort(sort_mode);
         wrapper.innerHTML = "";
         sortedListings.forEach(e => wrapper.appendChild(e));
     }
-    if (faves_toggle == true) {
-        sort_faves_func();
-    }
-    last_mode = algo;
+    display_faves();
+    last_mode = sort_mode;
 };
 
-
+let fave_toggle_display = () => {
+    let fave_btn = document.getElementById("sort_faves");
+    fave_btn.innerHTML = faves_toggle ? "&#10084;" : "&#128420;";
+}
 
 let toggle_faves = () => {
-    let fave_btn = document.getElementById("sort_faves");
-    if (document.title == "Loading...") {
-        return;
-    }
-    if (faves_toggle == false) {
-        fave_btn.innerHTML = "&#10084;";
-        sort_faves_func();
-        faves_toggle = true;
-    } else {
-        fave_btn.innerHTML = "&#128420;";
-        faves_toggle = false;
-        sort_func(last_mode);
-    }
+    faves_toggle = faves_toggle ? false : true
+    fave_toggle_display();
+    sort_func();
     $.cookie("faves_toggle", faves_toggle.toString(), { expires: 9999 });
 };
 
 
-let sort_faves_func = () => {
+let display_faves = () => {
+    if (!faves_toggle) return;
     let listingsArray = get_listings();
     if (listingsArray.length) {
         wrapper.innerHTML = "";
@@ -134,14 +146,8 @@ let sort_faves_func = () => {
         });
     }
 };
-let sort_toggle_func = () => {
-    if (faves_toggle == false) {
-        sort_func(last_mode);
-    } else {
-        sort_func(last_mode);
-        sort_faves_func();
-    }
-};
+
+
 
 
 function timer(element_id) {
@@ -165,85 +171,42 @@ function timer(element_id) {
 }
 
 
-// current_season = body.pop();
-// document.title = current_season;
-// prev_season_url = body.pop();
-// next_season_url = body.pop();
-// prev = document.getElementById("prev");
-// next = document.getElementById("next");
-// prev.value = prev_season_url;
-// next.value = next_season_url;
 
-let fave_toggle_cookie = $.cookie("faves_toggle");
-if (fave_toggle_cookie == null) {
-    $.cookie("faves_toggle", "false", { expires: 9999 });
-}
-// sort_func(score_sort); // default sort when first loading page
 
-let faves_cookie = $.cookie("faves");
-if (faves_cookie == null) {
-    $.cookie("faves", JSON.stringify(faves), { expires: 9999 });
-} else {
-    faves = JSON.parse($.cookie("faves"));
-}
-
-var wrapper = document.getElementById("wrapper");
 function populate_table(past = false) {
     $.ajaxSetup({
         async: false
     });
-    document.title = `${seasons.at(curr_season % 4).toUpperCase()} ${curr_year}`
-    let compiled_series_arr = retrieve_season(curr_year, seasons.at(curr_season % 4))
+    document.title = `${get_season_text().toUpperCase()} ${curr_year}`
+    let compiled_series_arr = retrieve_season(curr_year, get_season_text())
     compiled_series_arr.forEach((e, i) => {
         listing_div = document.createElement("div");
-        listing_div.classList.add("mt-3");
-        listing_div.classList.add("mb-3");
         listing_div.classList.add("listing");
-        listing_div.style.borderRadius = "3px";
         listing_div.setAttribute("data-countdown", e[1]);
         listing_div.setAttribute("data-popularity", e[2]);
         listing_div.setAttribute("data-score", e[3]);
         img_a = document.createElement("a");
         img_a.href = e[5];
         thumb_div = document.createElement("div");
-        thumb_div.style.height = "185px";
-        thumb_div.style.width = "140px";
+        thumb_div.classList.add("thumb-div");
         thumb = document.createElement("img");
+        thumb.classList.add("thumb");
         thumb.setAttribute("src", e[4]);
-        thumb.style.maxHeight = "100%";
-        thumb.style.width = "100%";
-        thumb.style.position = "relative";
-        thumb.style.top = "50%";
-        thumb.style.transform = "translateY(-50%)";
         img_a.appendChild(thumb);
         thumb_div.appendChild(img_a);
-        thumb_div.classList.add("d-inline-block");
         listing_div.appendChild(thumb_div);
         title = document.createElement("a");
+        title.classList.add("title");
         title.innerHTML = e[0];
-        title.style.maxWidth = "calc(100% - 44px)";
-        title.style.textDecoration = "none";
-        title.style.color = "inherit";
         title.href = e[5];
-        title.classList.add("d-inline-block");
-        title.classList.add("h2");
-        fave = document.createElement("h2");
-        if (faves.includes(e[0])) {
-            fave.innerHTML = "&#10084;";
-        } else {
-            fave.innerHTML = "&#128420;";
-        }
+        fave = document.createElement("span");
+        fave.innerHTML = faves.includes(e[0]) ? "&#10084;" : "&#128420;";
         fave.classList.add("fave");
-        fave.classList.add("d-inline-block");
-        fave.style.float = "right";
         headdiv = document.createElement("div");
         headdiv.appendChild(title);
         headdiv.appendChild(fave);
         text_block_div = document.createElement("div");
-        text_block_div.classList.add("d-inline-block");
-        text_block_div.style.verticalAlign = "middle";
-        text_block_div.style.paddingLeft = "20px";
-        text_block_div.style.width = "calc(100% - 160px)";
+        text_block_div.classList.add("text-block-div");
         text_block_div.appendChild(headdiv);
         span1 = document.createElement("span");
         span1.classList.add("lead");
@@ -264,56 +227,46 @@ function populate_table(past = false) {
         text_block_div.appendChild(watch_btn);
         text_block_div.appendChild(span3);
         listing_div.appendChild(text_block_div);
-        listing_div.style.width = "100%";
-        listing_div.style.margin = "0 auto";
-        listing_div.style.overflow = "hidden";
         wrapper.appendChild(listing_div);
         seconds_arr.push(e[1]);
-        if (!e[6] && past) {
+        if (past) {
             listing_div.setAttribute("data-countdown", 10 ** 10);
-            span3.innerHTML = "Finished Airing";
+            span3.innerHTML = "";
             return;
         }
-        var countdownTimer = setInterval(() => {
+        let countdownTimer = setInterval(() => {
             timer(i);
         }, 1000);
         functions_arr.push(countdownTimer);
     });
-
-    sort_func(last_mode);
+    sort_func();
     $.ajaxSetup({
         async: true
     });
 }
-populate_table();
-remain_faves_toggle = ($.cookie("faves_toggle") === "true");
-if (remain_faves_toggle) {
-    toggle_faves();
-}
 
+populate_table();
 
 function reset_page() {
     $("#wrapper").empty();
     functions_arr.forEach(element => {
         clearInterval(element);
     });
+    functions_arr = [];
 }
 
 $("#next-btn").on("click", function () {
     reset_page()
     curr_season += 1;
-    if (curr_season % 3 == 1) curr_year += 1;
-    console.log(curr_season % 3)
+    if (get_season_text() == "winter") curr_year += 1;
     populate_table();
-
 })
 
 $("#prev-btn").on("click", function () {
     reset_page()
     curr_season -= 1;
-    if (curr_season % 4 == -1) curr_year -= 1;
+    if (get_season_text() == "fall") curr_year -= 1;
     populate_table(true);
-
 })
 
 $("#curr-btn").on("click", function () {
@@ -325,25 +278,22 @@ $("#curr-btn").on("click", function () {
 
 
 $(document).on("click", ".fave", event => {
-    var fave = $(event.target).prev().html();
-    var heart = $(event.target);
-    if (faves.includes(fave)) {
-        let index = faves.indexOf(fave);
+    let title = $(event.target).prev().html();
+    let heart = $(event.target);
+    if (faves.includes(title)) {
+        let index = faves.indexOf(title);
+        // Remove from faves
         if (index > -1) {
             faves.splice(index, 1);
             heart.html("&#128420;");
         }
     } else {
-        faves.push(fave);
+        faves.push(title);
         heart.html("&#10084;");
     }
-    sort_toggle_func();
-    if (faves_toggle == true) {
-        sort_faves_func();
-    }
     $.cookie("faves", JSON.stringify(faves), { expires: 9999 });
+    sort_func();
 });
-
 
 
 let search_function = () => {
@@ -361,10 +311,11 @@ let search_function = () => {
 };
 
 document.addEventListener("DOMContentLoaded", function () {
+    fave_toggle_display();
+    toggle_search_box();
     el_autohide = document.querySelector(".autohide");
     navbar_height = document.querySelector(".navbar").offsetHeight;
     document.body.style.paddingTop = navbar_height + "px";
-
     if (el_autohide) {
         var last_scroll_top = 0;
         window.addEventListener("scroll", function () {
@@ -380,50 +331,58 @@ document.addEventListener("DOMContentLoaded", function () {
             last_scroll_top = scroll_top;
         });
     }
-
-    hideSearch();
-});
-window.addEventListener("resize", function (event) {
-    hideSearch();
-});
-function hideSearch() {
-    let bu = document.getElementById("buttons");
-    let nav = document.getElementById("nav");
-    let search_box = document.createElement("input");
-    search_box.classList.add("form-control-sm");
-    search_box.style.border = 0;
-    search_box.style.background = "none";
-    search_box.id = "search_box";
-    search_box.onkeyup = search_function;
-    search_box.type = "text";
-    search_box.placeholder = "Search";
-    let real_box = document.getElementById("search_box");
-    if (nav.offsetWidth > 700) {
-        if (!real_box) {
-            bu.appendChild(search_box);
-        }
-        else {
-            real_box.style.display = "inline";
-        }
-    } else if (real_box) {
-        real_box.style.display = "none";
-    }
-}
-
-$(document).ready(function () {
     document.getElementById("sort_soonest").onclick = () => {
         sort_func(soonest_sort);
     };
+
     document.getElementById("sort_most_recent").onclick = () => {
         sort_func(recent_sort);
     };
+
     document.getElementById("sort_highest_score").onclick = () => {
         sort_func(score_sort);
     };
+
     document.getElementById("sort_popular").onclick = () => {
         sort_func(pop_sort);
     };
+
     document.getElementById("sort_faves").onclick = () => {
         toggle_faves();
     };
+});
+
+
+let search_box = document.createElement("input");
+let navbar_div = document.getElementById("navbar-div");
+search_box.classList.add("form-control-sm");
+search_box.style.border = 0;
+search_box.style.background = "none";
+search_box.id = "search_box";
+search_box.onkeyup = search_function;
+search_box.type = "text";
+search_box.placeholder = "Search";
+navbar_div.appendChild(search_box);
+
+function toggle_search_box() {
+    let navbar = document.getElementById("nav");
+    let search_box = document.getElementById("search_box");
+    if (navbar.offsetWidth > 700) {
+        search_box.style.display = "inline";
+    } else {
+        search_box.style.display = "none";
+    }
+}
+
+let $search = $("#search_box");
+$(document).on("keydown", function (e) {
+    if (!$search.is(":focus"))
+        if (e.which != 9 && e.which != 13)
+            $search.focus();
+    if (e.which == 27)
+        $search.val("");
+});
+
+window.addEventListener("resize", function (event) {
+    toggle_search_box();
 });
